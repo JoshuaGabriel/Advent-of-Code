3eb07c2224cb4b73c3c836fc072d51cee34a4e73
JoshuaGabriel <42494981+JoshuaGabriel@users.noreply.github.com>
2023-12-05 14:27:48 -0800

    day 1 done

--- Day1/day1.py
+++ Day1/day1.py
@@ -22,6 +22,8 @@ class Solution:
         """
 
         res = 0  # sum of numbers
+        nums_to_word = {"one": 1, "two": 2, "three": 3, "four": 4,
+                        "five": 5, "six": 6, "seven": 7, 'eight': 8, "nine": 9, "zero": 0}
 
         for line in self.input:
             l = 0
@@ -33,23 +35,51 @@ class Solution:
             leftFound = False
             rightFound = False
 
+            left_word = ""
+            right_word = ""
+
             while (not leftFound or not rightFound):
 
                 if (not leftFound):
+
                     try:
                         left = int(line[l])
                         leftFound = True
                     except:
+                        left_word = left_word + line[l]
                         l += 1
+                    filtered_digit = self.brute_word_checker(left_word)
+
+                    if (filtered_digit != ""):
+                        left = nums_to_word[filtered_digit]
+                        leftFound = True
+
                 if (not rightFound):
+
                     try:
                         right = int(line[r])
                         rightFound = True
                     except:
+                        right_word = line[r] + right_word
                         r -= 1
+
+                    filtered_digit = self.brute_word_checker(right_word)
+                    if (filtered_digit != ""):
+                        right = nums_to_word[filtered_digit]
+                        rightFound = True
 
             res += int(str(left) + str(right))
         print(res)
+
+    def brute_word_checker(self, word):
+
+        digit_word = ["one", "two", "three", "four",
+                      "five", "six", "seven", 'eight', "nine"]
+
+        for w in digit_word:
+            if (w in word):
+                return w
+        return ""
 
 
 if __name__ == "__main__":
