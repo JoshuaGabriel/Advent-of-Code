49102f9050d7550f673fa121a6e974053c8f4f7b
JoshuaGabriel <42494981+JoshuaGabriel@users.noreply.github.com>
2023-12-06 12:39:15 -0800

    day3p2

--- Josh/Day3/day3.py
+++ Josh/Day3/day3.py
@@ -14,7 +14,7 @@ class Solution:
                 line = line.strip()
                 self.input.append(line)
 
-    def answer(self):
+    def part1(self):
         ROWS = len(self.input)
         COLS = len(self.input[0])
 
@@ -69,6 +69,50 @@ class Solution:
         elif (n == "."):
             return False
         return True
+
+    def answer(self):
+        ROWS = len(self.input)
+        COLS = len(self.input[0])
+
+        total = 0
+
+        def bfs(r, c):
+
+            res = 1
+            visited = set()
+            around = [(r+1, c), (r+1, c-1), (r+1, c+1),
+                      (r-1, c+1), (r-1, c), (r-1, c-1),
+                      (r, c+1), (r, c-1)]
+            count = 0
+            for x, y in around:
+
+                if (x >= ROWS or y >= COLS or x < 0 or y < 0 or (x, y) in visited):
+                    continue
+
+                if (self.isNum(self.input[x][y])):
+                    count += 1
+                    l, r = y-1, y+1
+                    num = self.input[x][y]
+                    visited.add((x, y))
+                    while (l >= 0 and self.isNum(self.input[x][l])):
+                        num = self.input[x][l] + num
+                        visited.add((x, l))
+                        l -= 1
+                    while (r < COLS and self.isNum(self.input[x][r])):
+                        num = num + self.input[x][r]
+                        visited.add((x, r))
+                        r += 1
+                    res = res * int(num)
+            if (count == 2):
+                return res
+            return 0
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (self.input[r][c] == "*"):
+                    total += bfs(r, c)
+
+        print(total)
 
 
 if __name__ == "__main__":
